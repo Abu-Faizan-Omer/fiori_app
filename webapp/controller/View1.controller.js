b99b7a23c7462bff6075b0a42126b5ef020d3a28
Abu Faizan Omer <abufaizan1997@gmail.com>
2026-04-01 11:18:38 +0000

    Change Ui element using Getter/Setter

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,6 +8,15 @@ sap.ui.define([
         },
         onPress: function(){
             this.getOwnerComponent().getRouter().navTo("RouteView2")
+        },
+        onSubmit:function(){
+            let name = this.getView().byId("idIpName").getValue()
+            let msg = "Welcome To "+ name
+            this.getView().byId("idTxtWelcome").setText(msg) 
+            this.getView().byId("idBtnSubmit").setType("Accept")
+            this.getView().byId("idTxtWelcome").setTextAlign("Left")
+            this.getView().byId("idLblName").setRequired(false)
+            this.getView().byId("idIpName").setEnabled(false)
         }
     });
 });
