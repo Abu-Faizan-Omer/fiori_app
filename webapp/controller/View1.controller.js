71b62c88107beecfb35de6b91a493e86962e0130
Abu Faizan Omer <abufaizan1997@gmail.com>
2026-04-02 09:33:31 +0000

    madatory field Validation

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,15 +9,28 @@ sap.ui.define([
         onPress: function(){
             this.getOwnerComponent().getRouter().navTo("RouteView2")
         },
-        onSubmit:function(){
-            let name = this.getView().byId("idIpName").getValue()
-            let msg = "Welcome To "+ name
-            this.getView().byId("idTxtWelcome").setText(msg) 
+        //    this is for dynamic ui
+        // onSubmit:function(){
+        //     let name = this.getView().byId("idIpName").getValue()
+        //     let msg = "Welcome To "+ name
+        //     this.getView().byId("idTxtWelcome").setText(msg) 
             
-            this.getView().byId("idBtnSubmit").setType("Accept")
-            this.getView().byId("idTxtWelcome").setTextAlign("Left")
-            this.getView().byId("idLblName").setRequired(false)
-            this.getView().byId("idIpName").setEnabled(false)
-        }
+        //     this.getView().byId("idBtnSubmit").setType("Accept")
+        //     this.getView().byId("idTxtWelcome").setTextAlign("Left")
+        //     this.getView().byId("idLblName").setRequired(false)
+        //     this.getView().byId("idIpName").setEnabled(false)
+        // }
+
+        //      this is for mandatory validation
+            onSubmit:function(){
+                let empId = this.getView().byId("idEmpId").getValue()
+
+                if(empId ===""){
+                    this.getView().byId("idEmpId").setValueState("Error")
+                    this.getView().byId("idEmpId").setValueStateText("Employee Id is Mandatory Please fill it")
+                }else{
+                    this.getView().byId("idEmpId").setValueState("None")
+                }
+            }
     });
 });
